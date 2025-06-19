cbc75cb5f6f912f43c78c0d8d8e1d78142b61dbc
tomomo086 <tomo.nfm08@gmail.com>
2025-06-19 19:56:31 +0900

    Update writer.py

--- excel/writer.py
+++ excel/writer.py
@@ -185,7 +185,7 @@ class ExcelWriter:
         
         self._set_time(ws, 'E41', other_times['patrol_4post'])
         self._safe_set_cell_value(ws, 'G41', patrol_data.post4_lastname)
-        self._safe_set_cell_value(ws, 'F41', "22:50")
+        self._safe_set_cell_value(ws, 'F41', "23:50")
         
         self._set_time(ws, 'H41', other_times['patrol_4post_end'])
         self._safe_set_cell_value(ws, 'J41', patrol_data.post4_lastname)
